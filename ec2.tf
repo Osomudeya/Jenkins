095ea916536241ccb855328d09e3ca4c153df024
Osomudeya <talk2osomudeya@gmail.com>
2023-02-14 02:04:20 +0100

    changes

--- ec2.tf
+++ ec2.tf
@@ -40,7 +40,7 @@ resource "aws_security_group" "ec2_security_group" {
     from_port        = 8080
     to_port          = 8080
     protocol         = "tcp"
-    cidr_blocks      = [0.0.0.0/0]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   # allow access on port 22
@@ -49,14 +49,14 @@ resource "aws_security_group" "ec2_security_group" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = [0.0.0.0/0]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
     from_port        = 0
     to_port          = 0
     protocol         = -1
-    cidr_blocks      = [0.0.0.0/0]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   tags   = {
