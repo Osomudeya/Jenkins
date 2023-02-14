e1b9a246b8cd15216d804d6a091ba1b50446f2ca
Osomudeya <talk2osomudeya@gmail.com>
2023-02-14 02:08:32 +0100

    i

--- ec2.tf
+++ ec2.tf
@@ -84,7 +84,7 @@ data "aws_ami" "amazon_linux_2" {
 
 # launch the ec2 instance and install website
 resource "aws_instance" "ec2_instance" {
-  ami                    = data.aws_ami.amazon_linux_2.vpc_id
+  ami                    = data.aws_ami.amazon_linux_2.id
   instance_type          = "t2.micro"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
