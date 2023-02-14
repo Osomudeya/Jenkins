1d90bc8a51cbf18cacea454516af74412dfc7f05
Osomudeya Zudonu <talk2osomudeya@gmail.com>
2023-02-14 02:25:11 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -88,7 +88,7 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = "t2.micro"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  key_name               = "kops2"
+  key_name               = "jenkins"
   # user_data            = file("install_jenkins.sh")
 
   tags = {
